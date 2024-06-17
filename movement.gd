2f3ca7a95aea4e0614a460efa3db7c430e9241d1
zigelzi <miika.a.savela@gmail.com>
2024-06-17 11:36:30 +0300

    Add max speed limit and camera following player

--- movement.gd
+++ movement.gd
@@ -1,9 +1,13 @@
 extends RigidBody2D
 
 @export var _movement_speed: int = 200
+@export var _max_speed = 800
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
 	var movement_direction: Vector2 = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	apply_impulse(movement_direction * _movement_speed)
+	apply_force(movement_direction * _movement_speed)
+	print(linear_velocity)
+
+func _integrate_forces(state):
+	if state.linear_velocity.length() > _max_speed:
+		state.linear_velocity = state.linear_velocity.normalized() * _max_speed
